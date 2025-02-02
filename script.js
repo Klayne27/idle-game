22702be59d0ece2ce3cff7735222f33e546e38e0
Wayne <kleavonwayne6270@gmail.com>
2025-02-02 20:04:15 +0100

    added timer functionality on clicking monsters

--- script.js
+++ script.js
@@ -15,25 +15,38 @@ let totalGoldGain = 0;
 
 const player = {
   gold: 4,
-
-  goldGain() {
-    setInterval(() => {
-      this.gold += totalGoldGain;
-      goldDisplay.textContent = `Gold: 💰${this.gold.toFixed(2)}`; 
-    }, 1000)
-  }
 };
 
-player.goldGain()
-
 class Monster {
-  constructor(name, price, baseGoldDrop, goldDrop, level, elementContainer) {
+  constructor(
+    name,
+    price,
+    baseGoldDrop,
+    goldDrop,
+    level,
+    elementContainer,
+    timerMulitplier
+  ) {
     (this.name = name),
       (this.price = price),
       (this.baseGoldDrop = baseGoldDrop),
       (this.goldDrop = goldDrop),
       (this.level = level),
       (this.elementContainer = elementContainer);
+    this.timerMulitplier = timerMulitplier;
+    this.isTimed = false;
+  }
+
+  clickMonster() {
+    if (this.isTimed) return;
+    this.isTimed = true;
+    const killTime = 1000 * this.timerMulitplier;
+
+    setTimeout(() => {
+      player.gold += this.goldDrop;
+      goldDisplay.textContent = `Gold: 💰${player.gold.toFixed(2)}`;
+      this.isTimed = false;
+    }, killTime);
   }
 
   buyLevel() {
@@ -41,13 +54,13 @@ class Monster {
       player.gold -= this.price;
       this.level++;
       this.goldDrop += this.baseGoldDrop;
-        totalGoldGain += this.baseGoldDrop
+      totalGoldGain += this.baseGoldDrop;
+      console.log(totalGoldGain);
       this.monsterScaling();
       this.updateUI();
     } else {
       console.log("Not enough gold!");
     }
-
   }
 
   monsterScaling() {
@@ -88,20 +101,37 @@ class Monster {
   }
 }
 
-const slime = new Monster("Slime", 4, 1, 0, 0, monContainer1);
-const rat = new Monster("Rat", 60, 60, 0, 0, monContainer2);
-const sprout = new Monster("Sprout", 720, 540, 540, 0, monContainer3);
-const imp = new Monster("Imp", 8640, 4320, 4320, 0, monContainer4);
-const hawk = new Monster("Hawk", 103680, 51840, 51840, 0, monContainer5);
-const hound = new Monster("Hound", 1244160, 622080, 622080, 0, monContainer6);
-const bear = new Monster("Bear", 14929920, 7464960, 7464960, 0, monContainer7);
+const slime = new Monster("Slime", 4, 1, 0, 0, monContainer1, 1);
+const rat = new Monster("Rat", 60, 60, 0, 0, monContainer2, 3);
+const sprout = new Monster("Sprout", 720, 540, 540, 0, monContainer3, 6);
+const imp = new Monster("Imp", 8640, 4320, 4320, 0, monContainer4, 12);
+const hawk = new Monster("Hawk", 103680, 51840, 51840, 0, monContainer5, 24);
+const hound = new Monster(
+  "Hound",
+  1244160,
+  622080,
+  622080,
+  0,
+  monContainer6,
+  96
+);
+const bear = new Monster(
+  "Bear",
+  14929920,
+  7464960,
+  7464960,
+  0,
+  monContainer7,
+  384
+);
 const werewolf = new Monster(
   "Werewolf",
   179159040,
   89579520,
   89579520,
   0,
-  monContainer8
+  monContainer8,
+  1536
 );
 const golem = new Monster(
   "Golem",
@@ -109,7 +139,8 @@ const golem = new Monster(
   1074954240,
   1074954240,
   0,
-  monContainer9
+  monContainer9,
+  6144
 );
 const troll = new Monster(
   "Troll",
@@ -117,36 +148,67 @@ const troll = new Monster(
   29668737024,
   29668737024,
   0,
-  monContainer10
+  monContainer10,
+  36864
 );
 
 monContainer1
   .querySelector(".btn-levelup")
   .addEventListener("click", slime.buyLevel.bind(slime));
+monContainer1
+  .querySelector(".mon-level")
+  .addEventListener("click", slime.clickMonster.bind(slime));
 monContainer2
   .querySelector(".btn-levelup")
   .addEventListener("click", rat.buyLevel.bind(rat));
+monContainer2
+  .querySelector(".mon-level")
+  .addEventListener("click", rat.clickMonster.bind(rat));
 monContainer3
   .querySelector(".btn-levelup")
   .addEventListener("click", sprout.buyLevel.bind(sprout));
+monContainer3
+  .querySelector(".mon-level")
+  .addEventListener("click", sprout.clickMonster.bind(sprout));
 monContainer4
   .querySelector(".btn-levelup")
   .addEventListener("click", imp.buyLevel.bind(imp));
+monContainer4
+  .querySelector(".mon-level")
+  .addEventListener("click", imp.clickMonster.bind(imp));
 monContainer5
   .querySelector(".btn-levelup")
   .addEventListener("click", hawk.buyLevel.bind(hawk));
+monContainer5
+  .querySelector(".mon-level")
+  .addEventListener("click", hawk.clickMonster.bind(hawk));
 monContainer6
   .querySelector(".btn-levelup")
   .addEventListener("click", hound.buyLevel.bind(hound));
+monContainer6
+  .querySelector(".mon-level")
+  .addEventListener("click", hound.clickMonster.bind(hound));
 monContainer7
   .querySelector(".btn-levelup")
   .addEventListener("click", bear.buyLevel.bind(bear));
+monContainer7
+  .querySelector(".mon-level")
+  .addEventListener("click", bear.clickMonster.bind(bear));
 monContainer8
   .querySelector(".btn-levelup")
   .addEventListener("click", werewolf.buyLevel.bind(werewolf));
+monContainer8
+  .querySelector(".mon-level")
+  .addEventListener("click", werewolf.clickMonster.bind(werewolf));
 monContainer9
   .querySelector(".btn-levelup")
   .addEventListener("click", golem.buyLevel.bind(golem));
+monContainer9
+  .querySelector(".mon-level")
+  .addEventListener("click", golem.clickMonster.bind(golem));
 monContainer10
   .querySelector(".btn-levelup")
   .addEventListener("click", troll.buyLevel.bind(troll));
+monContainer10
+  .querySelector(".mon-level")
+  .addEventListener("click", troll.clickMonster.bind(troll));
